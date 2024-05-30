4a3a4d8df3b2eb015c881d5d14e830c327d717f4
eliot ceron <eliot-arana@outlook.com>
2024-05-30 11:31:15 +0200

    updates images

--- verify_nb_components.py
+++ verify_nb_components.py
@@ -17,7 +17,7 @@ pivot_data=pd.pivot_table(main, values='score_norm', index=['product_id'],
                        columns=['user_id'], fill_value=0)
 
 
-svd = TruncatedSVD(n_components=50, n_iter=7, random_state=42)
+svd = TruncatedSVD(n_components=25, n_iter=7, random_state=42)
 decomposed_matrix = svd.fit_transform(pivot_data)
 explained_variance = svd.explained_variance_ratio_
 explained_variance = 1 - explained_variance #have the exolained variance on the good order
@@ -26,5 +26,8 @@ nb_composants_to_save = np.argmax(explained_variance > 0.95) + 1 #result is 5
 
 plt.figure(figsize=(15, 6))
 plt.plot(explained_variance)
-plt.scatter(nb_composants_to_save -1, 0.95, c="r", label="Seuil pour garder 95 % du poids informationnel")
+plt.scatter(nb_composants_to_save -1, 0.96, c="r", label="Seuil pour garder 95 % du poids informationnel")
+
+plt.xlabel('SVD components')  # Ajout du label pour l'axe des x
+plt.ylabel('Information cumulée')  # Ajout du label pour l'axe des y
 plt.show()
